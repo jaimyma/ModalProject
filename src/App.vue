07ffd80f43ab1286dda8c17206e2d9b1b7776124
jaimyma <jaimy@direct-effect.nl>
2025-02-27 11:56:05 +0100

    Open modal
    Possible to open the modal on 2 different ways

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,32 @@
 <template>
-  <h1>{{ title }}</h1>
-  <Modal />
+  <div>
+    <h1>{{ title }}</h1>
+    <p>Welcome...</p>
+
+    <Teleport to=".modals" v-if="showModal">
+      <Modal theme="sale" @close="toggleModal">
+        <template v-slot:links>
+          <a href="#">Sign Up now</a>
+          <a href="#">More Info</a>
+        </template>
+        <h1>Jaimy Mathon</h1>
+        <p>Junior Developer</p>
+      </Modal>
+    </Teleport>
+
+    <Teleport to=".modals" v-if="showModalTwo">
+      <Modal theme="sale" @close="toggleModalTwo">
+        <template v-slot:small-text >
+            <p>This is a small text above everything</p>
+        </template>
+        <h1>Another modal form</h1>
+        <p>This is the second one opened with shift</p>
+      </Modal>
+    </Teleport>
+
+    <button @click.alt="toggleModal">Open Modal (alt)</button>
+    <button @click.shift="toggleModalTwo">Open Modal (shift)</button>
+  </div>  
 </template>
 
 <script>
@@ -13,14 +39,24 @@ export default {
   },
   data() {
     return {
-      title: "My First Vue App"
+      title: "My First Vue App",
+      showModal: false,
+      showModalTwo: false,
+    }
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
+    toggleModalTwo() {
+      this.showModalTwo = !this.showModalTwo
     }
   }
 }
 </script>
 
 <style>
-#app {
+#app, .modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
